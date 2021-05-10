d621c3f6b2d1e0b18104d36ea1617ddcc725076f
Halims <78205083+Rozakulhalim@users.noreply.github.com>
2021-05-11 06:58:58 +0700

    Update user_settings.py

--- tortoolkit/core/user_settings.py
+++ tortoolkit/core/user_settings.py
@@ -24,7 +24,7 @@ no = "❌"
 yes = "✅"
 # Central object is not used its Acknowledged 
 tordb = TorToolkitDB()
-header =  '<b>**TorToolKit** by <a href="https://github.com/yash-dk">YashDK</a></b>\n<u>USER SETTINGS MENU - v1</u>'
+header =  '<b>**TorToolKit** by <a href="https://t.me/MRojeck_Lim">★彡🅻🅸🅼🆉★彡</a></b>\n<u>USER SETTINGS MENU - v1</u>'
 async def handle_user_setting_callback(e):
     db = tordb
     sender_id = str(e.sender_id)
